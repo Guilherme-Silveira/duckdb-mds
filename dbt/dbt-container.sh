375ccc4337d6458fa0970dce437b420fec61e3dd
Guilherme-Silveira <guilhermesilveira.s@hotmail.com>
2023-03-27 00:05:49 -0300

    new profile

--- dbt/dbt-container.sh
+++ dbt/dbt-container.sh
@@ -2,5 +2,6 @@
 
 docker run --rm -it --mount type=bind,source=${PWD},target=/usr/app/dbt \
  --mount type=bind,source=${PWD}/profiles.yml,target=/root/.dbt/profiles.yml \
+ --port 8080:8080 \
  --env-file ${PWD}/.env \
  --entrypoint bash  guisilveira/dbt-duckdb
